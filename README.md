b6d9694275ae42251f2e83c1e0660f6ab8938964
utkarshgangwar <utkarshgangwar22@gmail.com>
2026-03-14 18:27:16 +0530

    decorators and controllers

---
+++ README.md
@@ -0,0 +1,13 @@
+## Install nest cli - 
+# npm i -g @nest/cli
+## run on dev env - 
+# npm run start:dev
+
+# Modules
+## Modules are great for organizing components
+## generate with cli cmd - 
+# nest generate module <name>
+
+## Create a controller file cmd:
+# nest generate controller profiles
+
---
+++ src/profiles/profiles.controller.ts
@@ -0,0 +1,12 @@
+import { Controller } from '@nestjs/common';
+
+/**
+ * The below code is a decorator
+ * Which is a higher order function
+ * Higher-order functions in JavaScript are functions that
+ * operate on other functions by either accepting one 
+ * or more functions as arguments 
+ * or returning a new function as a result.
+ */
+@Controller('profiles')
+export class ProfilesController {}
