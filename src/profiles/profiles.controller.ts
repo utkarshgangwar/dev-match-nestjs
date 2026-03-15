a7de9fc0eb636bfd43913167153d2e4cb5893a08
utkarshgangwar <utkarshgangwar22@gmail.com>
2026-03-15 18:25:17 +0530

    services completed

--- src/profiles/profiles.controller.ts
+++ src/profiles/profiles.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, Query, Param, Post, Body, Put, Delete, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { CreateProfileDto } from './dto/create-profile.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { ProfilesService } from './profiles.service';
@@ -7,50 +18,42 @@ import { ProfilesService } from './profiles.service';
  * The below code is a decorator
  * Which is a higher order function
  * Higher-order functions in JavaScript are functions that
- * operate on other functions by either accepting one 
- * or more functions as arguments 
+ * operate on other functions by either accepting one
+ * or more functions as arguments
  * or returning a new function as a result.
  */
 @Controller('profiles')
 export class ProfilesController {
-    constructor(private profileSerivce: ProfilesService){}
-    // GET /profiles
-    @Get()
-    findAll(@Query('location') location: string){
-        return this.profileSerivce.findAll();
-    }
+  // initialize our service in a constructor
+  constructor(private profileSerivce: ProfilesService) {}
+  // GET /profiles
+  @Get()
+  findAll(@Query('location') location: string) {
+    return this.profileSerivce.findAll();
+  }
 
-    // GET /profiles/:id
-    @Get(':id')
-    findOne(@Param('id') id: string){
-        return { id };
-    }
+  // GET /profiles/:id
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.profileSerivce.findOne(id);
+  }
 
-    // POST /profiles
-    @Post()
-    create(@Body() createProfileDto: CreateProfileDto){
-        return {
-            name: createProfileDto.name,
-            description: createProfileDto.description,
-        };
-    }
+  // POST /profiles
+  @Post()
+  create(@Body() createProfileDto: CreateProfileDto): CreateProfileDto {
+    return this.profileSerivce.create(createProfileDto);
+  }
 
-    // PUT  /profiles/:id
-    @Put(':id')
-    update(@Param('id') id: string, @Body() updateProfile: UpdateProfileDto){
-        return {
-            id: id,
-            name: updateProfile.name,
-            description: updateProfile.description,
-        }
-    }
+  // PUT  /profiles/:id
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateProfile: UpdateProfileDto) {
+    return this.profileSerivce.update(id, updateProfile);
+  }
 
-    // DELETE /profiles/:id
-    @Delete(':id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    remove(@Param('id') id: string){
-        return {
-            id: id
-        }
-    }
+  // DELETE /profiles/:id
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {
+    return this.profileSerivce.remove(id);
+  }
 }
--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { randomUUID } from 'crypto';
+import { CreateProfileDto } from './dto/create-profile.dto';
 
 @Injectable()
 export class ProfilesService {
   private profiles = [
     {
-      id: randomUUID(),
+      id: '001',
       name: 'Shweta Soni',
       description: `Looking for someone to merge with my heart. I'm a full-stack romantic who refactors 
             my feelings until they pass all tests. Bonus points if you can debug my issues while we 
@@ -26,7 +27,44 @@ export class ProfilesService {
     },
   ];
 
-  findAll(){
+  findAll() {
     return this.profiles;
+  }
+
+  findOne(id: string) {
+    return this.profiles.find((item) => item.id === id);
+  }
+
+  create(data: CreateProfileDto) {
+    const newData = {
+      id: randomUUID(),
+      ...data,
+    };
+
+    this.profiles.push(newData);
+    return newData;
+  }
+
+  update(id: string, data: CreateProfileDto) {
+    const foundIndex = this.profiles.findIndex((profile) => profile.id === id);
+    if (foundIndex > -1) {
+      data.name && (this.profiles[foundIndex]['name'] = data?.name);
+      data.description &&
+        (this.profiles[foundIndex]['description'] = data.description);
+      return this.profiles[foundIndex];
+    } else {
+      return {};
+    }
+  }
+
+  remove(id: string) {
+    // return this.profiles.filter((profile) => profile.id === id);
+    const matchingProfileIndex = this.profiles.findIndex(
+      (profile) => profile.id === id,
+    );
+
+    if (matchingProfileIndex > -1) {
+      this.profiles.splice(matchingProfileIndex, 1);
+    }
   }
 }
